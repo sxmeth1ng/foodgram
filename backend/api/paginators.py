8e548b6abfcbc4dcc4f4812723f44583af052f23
Mikhail Kitaev <accforwarface@gmail.com>
2025-10-24 14:51:09 +0300

    update

--- backend/api/paginators.py
+++ backend/api/paginators.py
@@ -1,8 +1,10 @@
 from rest_framework.pagination import PageNumberPagination
 
+from recipes.constants import PAGINATOR_SIZE
+
 
 class BasePaginator(PageNumberPagination):
     """Пагинатор для пользователей и рецептов."""
 
     page_size_query_param = 'limit'
-    page_size = 10
+    page_size = PAGINATOR_SIZE
