705690ce44db037f9af4eca100af1ead63aa1bdb
Mikhail Kitaev <accforwarface@gmail.com>
2025-11-05 16:06:20 +0300

    fix urls and media

--- backend/api/views.py
+++ backend/api/views.py
@@ -9,7 +9,6 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from djoser import views as djoser_views
-from django.http import HttpResponseRedirect
 
 
 from api.filters import IngredientFilter, RecipeFilter
